aa25e341a63504bb2d9b806022b72967ffd3b68e
acm <academylandnet@gmail.com>
2022-04-23 16:33:23 +0430

    42 use asycData

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
-    <ul>
+    <div v-if="pending">loading...</div>
+    <ul v-else>
       <li v-for="(item, index) in data" :key="index">
-        {{ item.title }}
-      </li>
-    </ul>
-    <ul>
-      <li v-for="(item, index) in mountedData" :key="index">
         {{ item.title }}
       </li>
     </ul>
@@ -14,11 +10,9 @@
 </template>
 
 <script setup lang="ts">
-const { data, pending, error, refresh } = useFetch(
-  "https://api.nuxtjs.dev/mountains"
+const { data, pending, error, refresh } = useLazyAsyncData(
+  "home-mountains",
+  () => $fetch("https://api.nuxtjs.dev/mountains"),
+  {}
 );
-const mountedData = ref([]);
-onMounted(async () => {
-  mountedData.value = await $fetch("https://api.nuxtjs.dev/mountains");
-});
 </script>
