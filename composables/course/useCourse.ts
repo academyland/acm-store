b0b310fff5de2b753477c27740a2e7c2da0f38a1
acm <academylandnet@gmail.com>
2023-02-04 20:19:37 +0330

    add useErrorHandler to useCourse

--- composables/course/useCourse.ts
+++ composables/course/useCourse.ts
@@ -2,10 +2,11 @@ import { useCourseService } from "./useCourse.service";
 
 export const useCourseList = () => {
     const getCourseListFromApi = useCourseService();
-    const { data, pending } = useLazyAsyncData(
+    const { data, pending, error } = useLazyAsyncData(
         "course-list",
         () => getCourseListFromApi(),
         { server: true }
     );
+    useErrorHandler(error);
     return { data, pending }
 }
