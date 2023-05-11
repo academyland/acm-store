b1d1b6e7bd894bf7558e2dd1316dc3a1dad3a380
acm <academylandnet@gmail.com>
2023-05-11 19:05:18 +0330

    87 security

---
+++ pages/security.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+    security page
+    </div>
+    <a v-if="route.query.callback" :href="route.query.callback">{{route.query.callbackText}}</a>
+    <h1 v-html="route.query.title"></h1>
+   
+</template>
+<script lang="ts" setup>
+ const route = useRoute();
+
+</script>
